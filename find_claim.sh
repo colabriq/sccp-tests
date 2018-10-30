140846753a4ecf64fc9eb336a6f4ba380c55e556
Ian Maddison <ian@i.need.stronger.coffee>
2018-10-30 20:13:02 +0000

    Start using remote DHT again

--- find_claim.sh
+++ find_claim.sh
@@ -3,6 +3,5 @@
 curl -v \
   -H "Accept: application/xml" \
   -X GET \
-  -d "$DATA" \
   "http://localhost:8081/matches?sub=<https://twitter.com/jsmith76>&pre=<http://xmlns.com/foaf/0.1/name>"
   
