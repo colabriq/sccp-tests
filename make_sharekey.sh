8c682274e1e51551ca734986f58a47ebc11c6144
Ian Maddison <ian@i.need.stronger.coffee>
2018-11-27 01:12:33 +0000

    Add MPK as another attribute to sharkeys

--- make_sharekey.sh
+++ make_sharekey.sh
@@ -2,4 +2,4 @@
 
 curl -v \
   -H "Accept: application/json" \
-  -X GET "http://localhost:8081/share?sub=<https://twitter.com/ijmad8x>&pre=<http://xmlns.com/foaf/0.1/name>&start=1543273302&end=1543273304"
+  -X GET "http://localhost:8081/share?sub=<https://twitter.com/ijmad8x>&pre=<http://xmlns.com/foaf/0.1/name>&start=1514764800&end=1546300800"
