e4e162402d838dd4ff71703079c56b8d4c759d30
Ian Maddison <ian@i.need.stronger.coffee>
2018-11-28 12:34:43 +0000

    Rework stuff with sharekeys to properly cope with data types, tidy up

--- make_sharekey.sh
+++ make_sharekey.sh
@@ -2,4 +2,4 @@
 
 curl -v \
   -H "Accept: application/json" \
-  -X GET "http://localhost:8081/share?sub=<https://twitter.com/ijmad8x>&pre=<http://xmlns.com/foaf/0.1/name>&start=1514764800&end=1546300800"
+  -X GET "http://localhost:8081/share?sub=https://twitter.com/ijmad8x&pre=http://xmlns.com/foaf/0.1/name&start=1514764800&end=1546300800"
