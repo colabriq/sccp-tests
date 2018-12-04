4abb751645204ff47757ca562df0b09a6a6735ea
Ian Maddison <ian@i.need.stronger.coffee>
2018-12-04 20:49:07 +0000

    Add new lite demo compose file

---
+++ post_sparql_query_beef.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+read -r -d '' DATA <<'EOF'
+
+PREFIX com: <https://schemas.goodforgoodbusiness.com/common-operating-model/lite/>
+SELECT ?buyerRef ?quantity ?unitPrice ?shipmentRef ?ain ?vaccine WHERE {
+  ?order com:buyer <urn:uuid:448c5299-b858-4eb1-bc55-0a7a6c04efee>;
+    com:buyerRef ?buyerRef;
+    com:quantity ?quantity;
+    com:unitPrice ?unitPrice;
+    com:fulfilledBy ?shipment.
+  ?shipment com:consignee <urn:uuid:448c5299-b858-4eb1-bc55-0a7a6c04efee>;
+    com:shipmentRef ?shipmentRef.
+  OPTIONAL {
+    ?shipment com:usesItem ?cow.
+    OPTIONAL {
+      ?cow com:ain ?ain.
+      OPTIONAL {
+        ?cow com:vaccination ?vaccination.
+        ?vaccination com:vaccine ?vaccine.
+      }
+    }
+  }
+}
+
+EOF
+
+echo $VAR
+
+curl -v \
+  -H "Accept: application/xml" \
+  -H "Content-Type: application/sparql-query" \
+  -X POST \
+  -d "$DATA" \
+  "http://localhost:8080/sparql"
+  
