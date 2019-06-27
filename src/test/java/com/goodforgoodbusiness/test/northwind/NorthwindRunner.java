cc65efd1607b53b70e440a295ebf6a046a999209
Ian Maddison <ian@i.need.stronger.coffee>
2019-06-27 18:10:23 +0100

    Add the schema endpoint

--- src/test/java/com/goodforgoodbusiness/test/northwind/NorthwindRunner.java
+++ src/test/java/com/goodforgoodbusiness/test/northwind/NorthwindRunner.java
@@ -2,8 +2,6 @@ package com.goodforgoodbusiness.test.northwind;
 
 import static com.goodforgoodbusiness.shared.ConfigLoader.loadConfig;
 
-import java.net.URI;
-
 import com.goodforgoodbusiness.endpoint.EndpointModule;
 import com.goodforgoodbusiness.engine.EngineModule;
 import com.goodforgoodbusiness.shared.LogConfigurer;
@@ -11,17 +9,17 @@ import com.goodforgoodbusiness.test.module.EndpointTestRunner;
 import com.goodforgoodbusiness.test.module.EngineTestRunner;
 
 /** launches an endpoint that loads the beef turtle files directly */
-public class NorthwindEndpoint {
+public class NorthwindRunner {
 	public static void main(String[] args) throws Exception {
-		LogConfigurer.init(NorthwindEndpoint.class, "log4j.debug.properties");		
+		LogConfigurer.init(NorthwindRunner.class, "log4j.debug.properties");		
 		
-		// start components
+		var engineConfig = loadConfig(EngineTestRunner.class, "northwind/engine-1.properties");
+		new EngineModule(engineConfig).start();
 		
-		var engConfig1 = loadConfig(EngineTestRunner.class, "northwind/engine-1.properties");
-		new EngineModule(engConfig1).start();		
+		var endpointConfig = loadConfig(EndpointTestRunner.class, "northwind/endpoint-1.properties");
+		new EndpointModule(endpointConfig).start();
 		
-		var endUrl1 = new URI("http://localhost:8081");
-		var endConfig1 = loadConfig(EndpointTestRunner.class, "northwind/endpoint-1.properties");
-		new EndpointModule(endConfig1).start();
+		var schemaConfig = loadConfig(EndpointTestRunner.class, "northwind/schema.properties");
+		new EndpointModule(schemaConfig).start();
 	}
 }
